87c7a9a38256ee2aa40aed24914dc24f7e93e20f
mahesh_patapalli <mahesh.patapali@gmail.com>
2022-10-16 15:34:17 +0530

    gitflow-hotfix-stash: level_0_fix_1

--- Assets/scripts/CollisionHandler.cs
+++ Assets/scripts/CollisionHandler.cs
@@ -1,7 +1,29 @@
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class CollisionHandler : MonoBehaviour
 {
+=======
+    PlayerHealth playerHealth_script;
+    [SerializeField] float reduction_on_bump = 50f;
+    
+    private void Start() 
+    {
+        playerHealth_script = FindObjectOfType<PlayerHealth>();
+    }
+
+    void reduce_health()
+    {
+        bool is_dead = playerHealth_script.reduceHealth(reduction_on_bump);
+        if (is_dead)
+            reload_scene();
+    }
+    void reload_scene()
+    {
+        int current_scene_index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(current_scene_index);
+    }
+>>>>>>> Stashed changes
 
     private void OnCollisionEnter(Collision other) 
     {
@@ -21,6 +43,10 @@ public class CollisionHandler : MonoBehaviour
 
             default:
                 Debug.Log("not cool fam.");
+<<<<<<< Updated upstream
+=======
+                reload_scene();
+>>>>>>> Stashed changes
                 break;
         }
 
